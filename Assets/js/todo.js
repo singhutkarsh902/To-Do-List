c23d6ce997bd29d674c59bba483ce8ae906a0be5
singhutkarsh902 <singhutkarsh902@gmail.com>
2020-03-18 12:02:58 +0530

    Updated todo.js

--- Assets/js/todo.js
+++ Assets/js/todo.js
@@ -1,6 +1,23 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
-$("span").click(function() {
-	alert("!");
+
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(function() {
+		$(this).remove();
+	})
+	event.stopPropagation();
+})
+
+$("input[type='text'").keypress(function(event) {
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+		$(this).val("");
+	}
+
+})
+
+$(".fa-plus").on("click", function() {
+	$("input[type='text'").fadeToggle();
 })
